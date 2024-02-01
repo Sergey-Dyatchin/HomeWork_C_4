d64f59954a423e0794bee2dfac8190b8f02ca7d8
Sergey Dyatchin <s.dyatchin1983@gmail.com>
2024-02-01 23:17:54 +0300

    решил вторую задачу

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,16 @@
-﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+﻿// Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные
+
+string RequestLine()
+{
+    Console.Write("Введите строку: ");
+    string text = Console.ReadLine();
+    return text;
+}
+
+string ConvertLineLover(string text)
+{
+    text = text.ToLower();
+    return text;
+}
+
+Console.WriteLine($"Преобразованный текст: {ConvertLineLover(RequestLine())} ");
