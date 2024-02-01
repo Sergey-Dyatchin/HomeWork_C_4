0492bdeb4ef74409785ff39d27eb6d492ee60f3b
Sergey Dyatchin <s.dyatchin1983@gmail.com>
2024-02-01 23:02:44 +0300

    решил первую задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,1 +1,20 @@
 ﻿// Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.
+
+string GetStringFromCharArray (char[,] array)
+{
+    string result = string.Empty; 
+    foreach (char symbol in array) 
+    {
+       result += symbol;
+    }
+    return result;
+}
+
+char[,] chars = {
+                {'1', 'd', '!', '2', 'f'},
+                {'4', 'k', 't', '8', 'x'},
+                {'l', '0', ';', '5', 'h'},
+                {'e', 't', 'o', '4', '.'}
+                };
+string res = GetStringFromCharArray(chars);
+Console.WriteLine($"Result: {res}");
