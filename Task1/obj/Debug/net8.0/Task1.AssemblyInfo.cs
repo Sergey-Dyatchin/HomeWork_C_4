953c30f5f8d7a810f71a84ddcef03e558942a661
Sergey Dyatchin <s.dyatchin1983@gmail.com>
2024-02-01 23:49:08 +0300

    решил третью задачу

--- Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0492bdeb4ef74409785ff39d27eb6d492ee60f3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d64f59954a423e0794bee2dfac8190b8f02ca7d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,1 +1,22 @@
 ﻿// Задайте произвольную строку. Выясните, является ли она палиндромом.
+string RequestLine()
+{
+    Console.Write("Введите строку: ");
+    string text = Console.ReadLine();
+    return text;
+}
+
+bool PalindromeChecking (string text)
+{
+    text = text.ToLower();
+    for (int i = 0,  j = text.Length - 1; i < text.Length / 2; i++, j--)
+    {
+        if (text[i] != text[j])
+        {
+            return false;
+        }
+    }
+    return true;
+}
+Console.WriteLine((PalindromeChecking(RequestLine()))? "Строка является палиндромом":"Строка не является палиндромом");
+
